4838f3c1f2cde66874b593f304f688638de13ec2
fscarmen2 <fs.carmen@gmail.com>
2023-05-23 13:43:15 +0000

    Upgrade http-proxy-middleware to 3.0.0-beta.0

--- files/server.js
+++ files/server.js
@@ -5,14 +5,14 @@ const express = require("express");
 const app = express();
 var exec = require("child_process").exec;
 const os = require("os");
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const { legacyCreateProxyMiddleware } = require("http-proxy-middleware");
 var request = require("request");
 var fs = require("fs");
 var path = require("path");
 const auth = require("basic-auth");
 
 app.get("/", function (req, res) {
-  res.send("hello world");
+  res.status(200).send("hello world");
 });
 
 // 页面访问密码
@@ -109,17 +109,30 @@ function keep_web_alive() {
 }
 setInterval(keep_web_alive, 10 * 1000);
 
-app.use(
-  "/",
-  createProxyMiddleware({
-    changeOrigin: true, // 默认false，是否需要改变原始主机头为目标URL
-    onProxyReq: function onProxyReq(proxyReq, req, res) {},
-    pathRewrite: {
-      // 请求中去除/
-      "^/": "/"
+app.use( /* 具体配置项迁移参见 https://github.com/chimurai/http-proxy-middleware/blob/master/MIGRATION.md */
+  legacyCreateProxyMiddleware({
+    target: 'http://127.0.0.1:8080/', /* 需要跨域处理的请求地址 */
+    ws: true, /* 是否代理websocket */
+    changeOrigin: true, /* 是否需要改变原始主机头为目标URL,默认false */ 
+    on: {  /* http代理事件集 */ 
+      proxyRes: function proxyRes(proxyRes, req, res) { /* 处理代理请求 */
+        // console.log('RAW Response from the target', JSON.stringify(proxyRes.headers, true, 2)); //for debug
+        // console.log(req) //for debug
+        // console.log(res) //for debug
+      },
+      proxyReq: function proxyReq(proxyReq, req, res) { /* 处理代理响应 */
+        // console.log(proxyReq); //for debug
+        // console.log(req) //for debug
+        // console.log(res) //for debug
+      },
+      error: function error(err, req, res) { /* 处理异常  */
+        console.warn('websocket error.', err);
+      }
     },
-    target: "http://127.0.0.1:8080/", // 需要跨域处理的请求地址
-    ws: true // 是否代理websockets
+    pathRewrite: {
+      '^/': '/', /* 去除请求中的斜线号  */
+    },
+    // logger: console /* 是否打开log日志  */
   })
 );
 
